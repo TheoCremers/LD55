561161749047d01219c87b6e29e8e6fc43754187
Ruud <ruud88@gmail.com>
2024-04-13 21:29:15 +0200

    projectiles now destroy and damage

--- Assets/Scripts/FieldUnits/Projectile.cs
+++ Assets/Scripts/FieldUnits/Projectile.cs
@@ -6,6 +6,10 @@ public class Projectile : MonoBehaviour
 {
     private Rigidbody2D _rigidBody2D;
 
+    public int damage;
+
+    public bool isPlayerFaction;
+
     void Start()
     {
         _rigidBody2D = GetComponent<Rigidbody2D>();
@@ -22,6 +26,22 @@ public class Projectile : MonoBehaviour
         {
             var angle = Mathf.Atan2(_rigidBody2D.velocity.y, _rigidBody2D.velocity.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+    
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.TryGetComponent(out FieldUnit fieldUnit))
+        {
+            if (fieldUnit.isPlayerFaction != isPlayerFaction)
+            {
+                fieldUnit.TakeDamage(damage);
+                Destroy(gameObject);
+            }
+        } 
+        else if (collision.CompareTag("DestroyProjectiles"))
+        {
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Scripts/FieldUnits/SingleTargetRangedAttack.cs
+++ Assets/Scripts/FieldUnits/SingleTargetRangedAttack.cs
@@ -5,21 +5,28 @@ namespace FieldUnits
 {
     public class SingleTargetRangedAttack : Attack
     {
-        public GameObject projectilePrefab;
+        public Projectile projectilePrefab;
         public float projectileSpeed = 1f;
         private float _launchForce = 10f;
+        private FieldUnit _parent;
         public bool arcingShot = true;
+
+        private void Awake()
+        {
+            _parent = gameObject.GetComponent<FieldUnit>();
+        }
+
         public override void PerformAttack(FieldUnit target)
         {
             // Shoot projectile 
             FireProjectile(transform.position, target.transform.position);
-            // Delay
-            target.TakeDamage(damage);
         }
         
         private void FireProjectile(Vector2 startPosition, Vector2 targetPosition)
         {
             var projectile = Instantiate(projectilePrefab, startPosition, Quaternion.identity);
+            projectile.damage = damage;
+            projectile.isPlayerFaction = _parent.isPlayerFaction;
             var rb = projectile.GetComponent<Rigidbody2D>();
 
             if (arcingShot)
