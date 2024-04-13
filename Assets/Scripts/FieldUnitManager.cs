4e388ff198dc6f140f49b2cabfcaf4b64831b094
Ruud <ruud88@gmail.com>
2024-04-13 13:34:08 +0200

    fieldUnit opzet

---
+++ Assets/Scripts/FieldUnitManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldUnitManager
+{
+    public static List<FieldUnit> FieldUnits = new List<FieldUnit>();
+}
